f60f5356d32ba0dbda60ff14f35bc40159d246d9
dideey <bradleyjd32@gmail.com>
2023-11-24 19:17:26 +0300

    wr

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,11 @@
 //creates a file
 
 file { '/tmp/school':
-  ensure  => 'file',
+  ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
 
 }
+
