1c722bf22b3b84b9521cf250170e410f17ed24e1
dideey <bradleyjd32@gmail.com>
2023-11-29 05:07:27 +0300

    pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+#automation with puppet
+package { 'nginx':
+    ensure => installed,
+}
+
+file_line { 'install':
+    ensure => 'present',
+    path => '/etc/nginx/sites-enabled/default',
+    after => 'listen 80 default_server;',
+    line => 'rewrite ^/redirect_me https://www.youtube.com/channel/UC9YcTIQuhwgoOQqYMKYqW9A permanent;',
+}
+
+file { '/var/www/html/index.html':
+    content => 'Hello World!',
+}
+
+service { 'nginx':
+    ensure => 'running',
+    require => package['nginx'],
+}
