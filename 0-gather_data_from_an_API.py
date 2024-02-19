95fb59773d976cd538957c582f1d90d242004fe6
dideey <121872052+dideey@users.noreply.github.com>
2024-02-19 23:24:15 +0300

    Add files via upload

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""importing the libraries
+"""
+
+import requests
+import sys
+
+""""Gathering data from an API"""
+
+
+def get_user(employee_id):
+    """get user details"""
+    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(api_url)
+    user = response.json()
+    return user
+
+
+def get_todo(employee_id):
+    """get the info about todo list progress"""
+    u = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    response = requests.get(u)
+    todos = response.json()
+    return todos
+
+
+def main():
+    """calculating the progress"""
+    """Check if an argument is provided"""
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    """Get the employee ID from the command line argument"""
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Error: Please provide a valid integer for employee ID.")
+        sys.exit(1)
+
+    """Call the function to get user details"""
+    user = get_user(employee_id)
+    name = user.get('name', f"User {employee_id}")
+
+    """Call the function to get the TODO list"""
+    todos = get_todo(employee_id)
+
+    """total_task"""
+    total = len(todos)
+    """"task_completed"""
+    task_c = sum(1 for todo in todos if todo['completed'])
+
+    """output"""
+    print(f"Employee {name} is done with tasks({task_c}/{total}): ")
+
+    """"printing list of done tasks"""
+    for todo in todos:
+        if todo['completed']:
+            print(f"     {todo['title']}")
+
+
+if __name__ == "__main__":
+    main()
