76f0e00bd2e985b040a4d543d1af494e6df3e478
dideey <bradleyjd32@gmail.com>
2024-03-10 15:44:01 +0300

    error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """"getting number of subs"""
 import requests
-from sys import argv
+
 
 def number_of_subscribers(subreddit):
     """returns number of subscribers  for a subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
         return 0
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1]) 
