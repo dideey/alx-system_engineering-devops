1b647948c7e9947eaa5e1e70afe894051c33710b
dideey <bradleyjd32@gmail.com>
2024-03-18 01:31:59 +0300

    error handling

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """"getting number of subs"""
-from requests import get
-from sys import argv
+import requests
+
 
 def number_of_subscribers(subreddit):
-    """returns the number of subs"""
-    header = {
-        "User-Agent": "alx:lin.api.adv:v1.0.0 (by /u/gena)" 
-    }
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=header).json()
-    try:
-        return count.get("data").get("subscribers")
-    except:
+    """returns number of subs"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    if response.status_code != 200:
         return 0
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1]) 
+    else:
+        return data['data']['subscribers']
