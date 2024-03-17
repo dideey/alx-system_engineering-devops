74c9b5555e1a64ae901c58d840ab985e52568c25
dideey <bradleyjd32@gmail.com>
2024-03-18 01:36:50 +0300

    error handling

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""""getting top ten posts"""
+"""
+   prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the
-    first 10 hot posts listed for a given subreddit.
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-    else:
-        for i in range(10):
-            print(response.json().get('data').get('children')[i].get
-                  ('data').get('title'))
+    """Function that fetches top 10 gists"""
+    apiUrl = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    userAgent = "Mozilla/5.0"
+    limits = 10
+
+    response = requests.get(
+        apiUrl, headers={"user-agent": userAgent}, params={"limit": limits})
+    if not response:
+        print('None')
+        return
+    response = response.json()
+    list_obj = response['data']['children']
+    for obj in list_obj:
+        print(obj['data']['title'])
+    return
