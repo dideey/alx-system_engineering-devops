ce8a2c08ea280b4ce663b08abedadc693b68ce5f
dideey <bradleyjd32@gmail.com>
2024-03-18 01:38:20 +0300

    error handling

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""
-   prints the titles of the first 10 hot posts listed for a given subreddit
-"""
-
+"""function that prints posts"""
 import requests
 
-
 def top_ten(subreddit):
-    """Function that fetches top 10 gists"""
-    apiUrl = "https://reddit.com/r/{}/hot.json".format(subreddit)
-    userAgent = "Mozilla/5.0"
-    limits = 10
-
-    response = requests.get(
-        apiUrl, headers={"user-agent": userAgent}, params={"limit": limits})
-    if not response:
-        print('None')
+    """printing titles of 10 hot posts"""
+    hot_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    header = {
+        "User-Agent": "alx:lin.api.adv:v1.0.0 (by /u/gena)"
+    }
+    param = {
+        "limit": 10
+    }
+    response = requests.get(hot_url, headers=header, params=param,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-    response = response.json()
-    list_obj = response['data']['children']
-    for obj in list_obj:
-        print(obj['data']['title'])
-    return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
